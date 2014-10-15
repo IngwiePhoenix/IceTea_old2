4277252c16bf39c784ea22a553e7cbd4da90a1ab
Ingwie Phoenix <ingwie2000@googlemail.com>
2014-10-15 04:24:35 +0200

    Now at the point where tasks are created. Pretty damn close. The step afterwards is the executor...wow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <list>
 #include <map>
+#include <vector>
+#include <stdlib.h>
 
 // Threading
 #include "tinythread.h"
@@ -55,13 +57,25 @@ Value::Object* rules;
 Value::Object* actions;
 
 // A task in C++ representation
+enum TaskType { SCRIPT, COMMAND };
 struct Task {
-    Value::Object* rule;
+    string ruleName;
+    string targetName;
+    vector<string> files;
+    string output;
+
+    TaskType type;
+    // [ cmd1, cmd2, ... ]
+    vector<string> commands;
+    // Rule.build: function(input, output, target)
+    // The function is always called with only one input, but access to the
+    // target is given for settings - such as include dirs and alike.
+    Value::Method* onBuild;
+
+    // The related target, pushed to the function.
     Value::Object* target;
-    Value::Array* input;
-    Value::String* output;
 };
-typedef WorkQueue<Task*> TaskQueue;
+typedef WorkQueue<Task*, OS*> TaskQueue;
 
 // Since OS is not thread safe, we have to make thread-safe accessors
 bool obj_access(string index, Value::Object* obj, Value::Var& output) {
@@ -91,10 +105,28 @@ void getFileListRec(Value::Array* arr, list<string> &str_list) {
     }
 }
 
+bool ruleExists(Value::String* ruleName, OS* os) {
+    os->pushValueById(ruleName->valueID);
+    os->pushValueById(rules->valueID);
+    if(os->in(/* string in object */)) {
+        os->pop(2);
+        return true;
+    } else {
+        os->pop(2);
+        return false;
+    }
+}
+
+// In order to call OS savely, we use this.
+tthread::mutex OSMutex;
+void lockOS() { OSMutex.lock(); }
+void unlockOS() { OSMutex.unlock(); }
+
 /* The task processors
    There are a few of them:
         - Initializer: If it exists, call init() on each target. Ommit this, if --help is used.
             * the init() function can add entries to the help screen. Important for configuration.
+            * Check if rules that targets reffer to, exist.
         - Preprocessors: Run configure on each object.
             * configure() has access to the config object, command line params (parsed) and can also abort.
             * After doing so, strech out their inputs if its a wildcard
@@ -118,6 +150,13 @@ bool Initializer(OS* os) {
         os->nextIteratorStep();
         Value::String key(os, -2);
         Value::Object val(os);
+        os->getProperty("_"); // Guarantee'd to exist.
+        Value::String ruleName(os);
+        if(!ruleExists(&ruleName, os)) {
+            cerr << "Target \"" << key << "\" refferences to non-existing rule \"" << ruleName << "\"." << endl;
+            return false;
+        }
+        os->pushValueById(val.valueID);
         os->pushString("init");
         if(os->in(-1, -2)) {
             os->popString();
@@ -129,6 +168,14 @@ bool Initializer(OS* os) {
                 cerr << "[IceTea]: The init() method in " << key << "is not a function! Found "
                      << os->getTypeStr().toChar() << " instead.";
             }
+        } else {
+            cerr << "IceTea: Error detected. Stack:" << endl
+                 << "    -1: " << os->getTypeStr(-1).toChar() << " --> " << os->toString(-1).toChar() << endl
+                 << "    -2: " << os->getTypeStr(-2).toChar() << " --> " << os->toString(-2).toChar() << endl
+                 << "    -3: " << os->getTypeStr(-3).toChar() << " --> " << os->toString(-3).toChar() << endl
+                 << "    -4: " << os->getTypeStr(-4).toChar() << " --> " << os->toString(-4).toChar() << endl
+                 << "    -5: " << os->getTypeStr(-5).toChar() << " --> " << os->toString(-5).toChar() << endl
+                 << endl;
         }
         // Cause IceTea to exit
         if(os->isExceptionSet()) return false;
@@ -172,11 +219,96 @@ bool Preprocessor(OS* os) {
     }
     return true;
 }
-bool Transformer(OS* os) {
-    return true;
+bool Transformer(OS* os, TaskQueue* queue) {
+    // The following blob is to emulate a possible, incomplete build logic.
+    /*
+    // Now that it has ran, it's time to take stuff apart.
+    Value::Object* icetea = (Value::Object*)(*targets)["IceTea"];
+    Value::Array* files = (Value::Array*)(*icetea)["input"];
+    list<string> fileList;
+    getFileListRec(files, fileList);
+    delete files;
+
+    // Let's try to call IceTea's configure
+    Value::Method cg(os, icetea, "configure");
+    cg(0,0);
+    // Now lets try to build it...staticaly for now.
+    Value::Object* cxx = (Value::Object*)(*rules)["cxx"];
+    Value::Method cxx_p(os, cxx, "prepare");
+    Value::Method cxx_b(os, cxx, "build");
+    Value::Object* exe = (Value::Object*)(*rules)["exe"];
+    Value::Method exe_p(os, exe, "prepare");
+    Value::Method exe_b(os, exe, "build");
+    // Call the two preparational methods first.
+    cxx_p(0,0);
+    exe_p(0,0);
+    // Now we "build"
+    os->newArray();
+    for(list<string>::iterator ft=fileList.begin(); ft!=fileList.end(); ++ft) {
+        os->pushString( (ft->c_str()) );
+        os->addProperty(-2);
+    }
+    cxx_b(1,0);
+    os->newArray();
+    for(list<string>::iterator ft2=fileList.begin(); ft2!=fileList.end(); ++ft2) {
+        os->pushString( (ft2->c_str()) );
+        os->addProperty(-2);
+    }
+    exe_b(1,0);
+    delete cxx;
+    delete exe;
+    delete icetea;
+    */
+
+    // And now lets get serious.
+    // We only must push "safe" targets into the queue...
+    lockOS(); // We are prepping targets. It must be completely locked first.
+    os->pushValueById(targets->valueID);
+    for(int i=os->getLen(); i>0; i--) {
+        os->nextIteratorStep();
+        Value::String key(os, -2);
+        Value::Object val(os);
+        Value::Array* input = (Value::Array*)val["input"];
+        Value::String* ruleName = (Value::String*)val["_"];
+        for(int j=input->length(); j>0; j--) {
+            // We walk the files backwards, its easier to maintain.
+            Value::String* v_file = (Value::String*)(*input)[j];
+            string file = (*v_file);
+            string rule = (*ruleName);
+            // Now we need to resolve the rules.
+        }
+    }
+    unlockOS();
+    return false;
 }
-int Run(OS* os) {
-    return 0;
+
+// Because Run() is our thread function, it needs some depdendencies
+// The needed struct is above.
+void Run(void* threadData) {
+    TaskQueue* tasks = (TaskQueue*)threadData;
+    OS* os = tasks->data["os"];
+    Task* task;
+    while(!tasks->hasToStop()) {
+        if(tasks->remove(task)) {
+            bool doContinue=true;
+            for(int e=0; e>task->files.size(); e++) {
+                if(!FileExists( task->files[e].c_str() )) {
+                    // Put the task back in, we cant run it now.
+                    tasks->add(task);
+                    doContinue=false;
+                }
+            }
+            if(doContinue) {
+                if(task->type == SCRIPT) {
+                    lockOS();
+                    // ...
+                    unlockOS();
+                } else {
+                    // Just run the commands at once.
+                }
+            }
+        }
+    }
 }
 
 // Handle errors properly.
@@ -196,12 +328,22 @@ int main(int argc, const char** argv) {
 
     // Arg stuff.
     CLI* cli = new CLI(argc, argv, "IceTea by Ingwie Phoenix");
+    // Fetch thread number beforehand!
+    stringstream thrs_sst;
+    thrs_sst << thread::hardware_concurrency();
     cli->group("Main options");
     cli->insert("-f", "--file", "<file>", "Select a different built file.", true, "./build.it");
     cli->insert("-b", "--boot", "<file>", "Select a different bootstrap file.", true, "./boot.it");
     cli->insert("-h", "--help", "", "Show this help.");
     cli->insert("-u", "--usage", "", "Show the usage details of a project plus this help.");
     cli->insert("-d", "--define", "key=value", "Define a global value within the scripting language's scope.");
+    cli->insert("", "--debug", "", "Run a debug build; rules and targets may use specialized settings for this build.");
+    cli->insert("-v", "--verbose", "", "Commands are shown instead of the progress indicator.");
+    cli->insert(
+        "-j", "--jobs", " ",
+        "Amount of jobs to run at the same time. Default: "+thrs_sst.str(),
+        true, thrs_sst.str()
+    );
     cli->insert("-t", "--target", "<target>", "Build only the specified target.");
     cli->parse();
 
@@ -216,6 +358,8 @@ int main(int argc, const char** argv) {
     // Pre-check
     if(cli->check("-u")) cli->group("Project options");
 
+    // Introduce bootstrap.it later...
+
     // Run the script to populate the global objects.
     if(FileExists(cli->value("-f").c_str()))
         os->require(cli->value("-f").c_str());
@@ -225,7 +369,9 @@ int main(int argc, const char** argv) {
     }
 
     // Call every target's init() method.
-    Initializer(os);
+    if(!Initializer(os)) {
+        goto itCleanup;
+    }
 
     // Post-check
     if(cli->check("-u")) {
@@ -233,51 +379,39 @@ int main(int argc, const char** argv) {
         goto itCleanup;
     } else {
         // Run configure and strech inputs
-        Preprocessor(os);
+        if(!Preprocessor(os)) goto itCleanup;
 
-        // The following blob is to emulate a possible, incomplete build logic.
-        /*
-        // Now that it has ran, it's time to take stuff apart.
-        Value::Object* icetea = (Value::Object*)(*targets)["IceTea"];
-        Value::Array* files = (Value::Array*)(*icetea)["input"];
-        list<string> fileList;
-        getFileListRec(files, fileList);
-        delete files;
+        // At this point, we can savely begin to create the needed things for threaded work.
+        int thrs;
+        stringstream tmp;
+        tmp << cli->value("-j");
+        tmp >> thrs;
+        // Now that we have the thread amount value, lets make it happen.
+        TaskQueue* tasks = new TaskQueue(thrs, Run);
 
-        // Let's try to call IceTea's configure
-        Value::Method cg(os, icetea, "configure");
-        cg(0,0);
-        // Now lets try to build it...staticaly for now.
-        Value::Object* cxx = (Value::Object*)(*rules)["cxx"];
-        Value::Method cxx_p(os, cxx, "prepare");
-        Value::Method cxx_b(os, cxx, "build");
-        Value::Object* exe = (Value::Object*)(*rules)["exe"];
-        Value::Method exe_p(os, exe, "prepare");
-        Value::Method exe_b(os, exe, "build");
-        // Call the two preparational methods first.
-        cxx_p(0,0);
-        exe_p(0,0);
-        // Now we "build"
-        os->newArray();
-        for(list<string>::iterator ft=fileList.begin(); ft!=fileList.end(); ++ft) {
-            os->pushString( (ft->c_str()) );
-            os->addProperty(-2);
+        // Save our OS instance to the thread stuff.
+        tasks->data["os"] = os;
+
+        // Now, we've got a waiting task runner there, we ned to feed it one by one.
+        if(!Transformer(os, tasks)) {
+            // Meep! We have to kill all the threads, at once.
+            // We can savely let it go to a fiish by stopping them.
+            tasks->stop();
+        } else {
+            // Run untill the list is empty... and then stop all threads.
+            tasks->drain();
+            tasks->stop();
         }
-        cxx_b(1,0);
-        os->newArray();
-        for(list<string>::iterator ft2=fileList.begin(); ft2!=fileList.end(); ++ft2) {
-            os->pushString( (ft2->c_str()) );
-            os->addProperty(-2);
-        }
-        exe_b(1,0);
-        delete cxx;
-        delete exe;
-        delete icetea;
-        */
+
+        // Make sure all threads come together first.
+        tasks->joinAll();
+        // Its now save to delete the pool!
+        delete tasks;
     }
 
     // Clean up.
     itCleanup:
+    delete cli;
     delete targets;
     delete rules;
     delete actions;
--- src/threading.h
+++ src/threading.h
@@ -6,6 +6,7 @@
 
 #include <iostream>
 #include <list>
+#include <map>
 #include <vector>
 
 using namespace std;
@@ -19,7 +20,7 @@ using namespace tthread;
 // Just a short typedef...
 typedef void (*ThreadFunction)(void*);
 
-template<typename T> class WorkQueue {
+template<typename T, typename ST=void*> class WorkQueue {
 private:
     vector<tthread::thread*> threads;
     int count;
@@ -31,18 +32,20 @@ public:
     // Synchronisation
     tthread::mutex               mutex;
     tthread::condition_variable  cond;
+    // Misc, inter-thread storage.
+    map<string, ST>              data;
 
-    WorkQueue(int amt, ThreadFunction func) : count(amt), myID(tthread::this_thread::get_id()), doStop(false) {
+    inline WorkQueue(int amt, ThreadFunction func) : count(amt), myID(tthread::this_thread::get_id()), doStop(false) {
         for(int i=0; i<amt; i++) {
             threads.push_back(new tthread::thread(func, (void*)this));
         }
     }
-    ~WorkQueue() {
+    inline ~WorkQueue() {
         for(int i=0; i<count; i++) {
             delete threads[i];
         }
     }
-    bool stillGoing() {
+    inline bool stillGoing() {
         if(myID != tthread::this_thread::get_id()) return true;
         bool isGoing=false;
         for(int i=0; i<count; i++) {
@@ -50,37 +53,41 @@ public:
         }
         return isGoing;
     }
-    void joinAll() {
+    inline void joinAll() {
         if(myID != tthread::this_thread::get_id()) return;
         for(int i=0; i<count; i++) {
             threads[i]->join();
         }
     }
-    void detachAll() {
+    inline void detachAll() {
         if(myID != tthread::this_thread::get_id()) return;
         for(int i=0; i<count; i++) {
             threads[i]->detach();
         }
     }
-    void drain() {
+    inline void drain() {
         if(myID != tthread::this_thread::get_id()) return;
         // Just a stub to wait for the data list to flail out again.
         while(size() != 0) { cond.notify_one(); }
     }
-    void stop() {
+    inline void stop() {
         doStop = true;
         cond.notify_all();
     }
+    inline bool hasToStop() {
+        // This might end up in a race condition...I am nto sure.
+        return doStop;
+    }
 
     // Add item to list's end.
-    void add(T item) {
+    inline void add(T item) {
         tthread::lock_guard<tthread::mutex> guard(mutex);
         queue.push_back(item);
         cond.notify_one();
     }
 
     // Get item from list's front.
-    bool remove(T& item) {
+    inline bool remove(T& item) {
         tthread::lock_guard<tthread::mutex> guard(mutex);
         while(queue.empty() && doStop != true) {
             cond.wait<tthread::mutex>(mutex);
@@ -89,14 +96,13 @@ public:
         if(doStop) return false;
 
         //if(!queue.empty()) {
-
             item = queue.front();
             queue.pop_front();
             return true;
         //} else return false;
     }
 
-    int size() {
+    inline int size() {
         tthread::lock_guard<tthread::mutex> guard(mutex);
         int size = queue.size();
         return size;
