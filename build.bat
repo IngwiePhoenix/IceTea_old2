33378775b687a02ee466c5b90ef2613c1b1ba8b1
Ingwie Phoenix <ingwie2000@googlemail.com>
2016-01-14 04:13:28 +0100

    Updated build files.

--- build.bat
+++ build.bat
@@ -8,8 +8,11 @@ mkdir out
 REM Build IncBin
 cl incbin/incbin.c /Feincbin
 
-Build the source container
-incbin src/scripts.rc -o src/scripts.cpp
+REM Build the source container
+incbin src/scripts.rc -o out/os-scripts.cpp
 
 REM Build IceTea
-cl /EHsc src/*.cpp /Feout\icetea
+cl /EHsc src\*.cpp out\os-scripts.cpp /Feout\icetea.flat
+
+REM Build it using ourself.
+.\out\icetea.flat --it-optimize
--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 # Set up CWD
 cd "$(dirname '$0')"
-set -v
+set -ev
 # Prepare speedbuild
 if [ ! -d "./out" ]; then
     mkdir out
 fi
 
 # Build the incbin utility
-gcc incbin/incbin.c -o out/incbin
+gcc deps/incbin/incbin.c -o out/incbin
 
 # Generate resources
-./out/incbin src/scripts.rc -o src/os-scripts.cpp
+./out/incbin src/scripts.rc -o out/os-scripts.cpp
 
 # Speedbuild IceTea
-CXXFLAGS="-Wno-switch $CXXFLAGS"
-g++ src/*.cpp -o out/speed.icetea $CXXFLAGS
+CXXFLAGS="-Wno-switch -pthread $CXXFLAGS"
+g++ -Isrc src/*.cpp out/os-scripts.cpp -o out/speed.icetea $CXXFLAGS
 
 # Build ourself
-./out/speed.icetea
+./out/speed.icetea --it-optimize
